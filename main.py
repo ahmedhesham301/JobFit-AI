e999cfa9df7cae22f9053fab9b109f5a88a0f5d1
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2025-10-02 17:23:44 +0300

    add remote,country,location

--- main.py
+++ main.py
@@ -23,20 +23,21 @@ with open("instruction.txt", "r") as f:
     CV = f.read()
 
 
-def get_jobs(job_title, results_wanted, hours_old):
+def get_jobs(job_title, results_wanted, hours_old, country, location, is_remote):
     global all_jobs
-    jobs = getJobs(job_title, results_wanted, hours_old)
+    jobs = getJobs(job_title, results_wanted, hours_old, country, location, is_remote=False)
     all_jobs = pd.concat([all_jobs, jobs], ignore_index=True)
 
 
 if __name__ == "__main__":
-    get_jobs("devops", results_wanted=30, hours_old=2)
-    get_jobs("backend", results_wanted=30, hours_old=2)
-    get_jobs("software engineer", results_wanted=30, hours_old=2)
-    get_jobs("cloud", results_wanted=30, hours_old=2)
-    get_jobs("site reliability engineer", results_wanted=30, hours_old=2)
-    get_jobs("sre", results_wanted=30, hours_old=2)
-    get_jobs("intern", results_wanted=30, hours_old=2)
+    get_jobs("devops", results_wanted=30, hours_old=2, country="egypt", location="cairo")
+    get_jobs("backend", results_wanted=30, hours_old=2, country="egypt", location="cairo")
+    get_jobs("software engineer",results_wanted=30,hours_old=2,country="egypt",location="cairo",)
+    get_jobs("cloud", results_wanted=30, hours_old=2, country="egypt", location="cairo")
+    get_jobs("site reliability engineer",results_wanted=30,hours_old=2,country="egypt",location="cairo")
+    get_jobs("sre", results_wanted=30, hours_old=2, country="egypt", location="cairo")
+    get_jobs("intern", results_wanted=30, hours_old=2, country="egypt", location="cairo")
+    # get_jobs("devops",results_wanted=200,hours_old=2,country="worldwide",location="",is_remote=True)
     all_jobs.drop_duplicates(inplace=True, ignore_index=True)
     logging.warning(f"Total jobs no duplicates: {len(all_jobs)}")
     filter_jobs(all_jobs, CV, api_keys, good_fit_jobs)
