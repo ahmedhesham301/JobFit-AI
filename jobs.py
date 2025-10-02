425411c200acd2e0f0e207bc4583299cf22d00d0
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2025-10-02 17:23:44 +0300

    add remote,country,location

--- jobs.py
+++ jobs.py
@@ -1,28 +1,37 @@
 from jobspy import scrape_jobs
 import logging
 
-def getJobs(jobTitle, results_wanted, hours_old):
+
+def getJobs(
+    jobTitle,
+    results_wanted,
+    hours_old,
+    country,
+    location,
+    is_remote,
+):
     jobs = scrape_jobs(
         site_name=[
             "indeed",
             "linkedin",
             # "zip_recruiter",
-            "google",
+            # "google",
             # "glassdoor",
             # "bayt",
             # "naukri",
             # "bdjobs",
         ],
         search_term=jobTitle,
-        location="Cairo",
+        location=location,
         results_wanted=results_wanted,
-        google_search_term=f"{jobTitle} jobs near Cairo since {hours_old} hours",
+        # google_search_term=f"{jobTitle} jobs near Cairo since {hours_old} hours",
         hours_old=hours_old,
-        country_indeed="Egypt",
+        country_indeed=country,
+        is_remote=is_remote,
         linkedin_fetch_description=True,  # gets more info such as description, direct job url (slower)
         # proxies=["208.195.175.46:65095", "208.195.175.45:65095", "localhost"],
     )
-    logging.warning(f"Found {len(jobs)} {jobTitle} jobs")
+    logging.warning(f"Found {len(jobs)} {jobTitle} jobs in {country},{location}")
     # print(jobs)
     return jobs
     # jobs.to_csv(
