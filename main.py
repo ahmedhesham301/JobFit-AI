032b1b041b70a90a5ffb9b9ce33bb6bac27307ed
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2025-10-06 00:02:31 +0300

    make the job fail after sending emails when all api keys are used

--- main.py
+++ main.py
@@ -28,23 +28,29 @@ def get_jobs(job_title, results_wanted, hours_old, country, location, is_remote=
     all_jobs = pd.concat([all_jobs, jobs], ignore_index=True)
 
 
+def main():
+    get_jobs("devops", results_wanted=30, hours_old=2, country="egypt", location="cairo")
+    # get_jobs("backend", results_wanted=30, hours_old=2, country="egypt", location="cairo")
+    # get_jobs("software engineer",results_wanted=30,hours_old=2,country="egypt",location="cairo",)
+    # get_jobs("cloud", results_wanted=30, hours_old=2, country="egypt", location="cairo")
+    # get_jobs("site reliability engineer",results_wanted=30,hours_old=2,country="egypt",location="cairo")
+    # get_jobs("sre", results_wanted=30, hours_old=2, country="egypt", location="cairo")
+    # get_jobs("intern", results_wanted=30, hours_old=2, country="egypt", location="cairo")
+    
+    # get_jobs("devops",results_wanted=200,hours_old=2,country="worldwide",location="",is_remote=True)
+    # get_jobs("backend",results_wanted=200,hours_old=2,country="worldwide",location="",is_remote=True)
+    # get_jobs("software engineer",results_wanted=200,hours_old=2,country="worldwide",location="",is_remote=True)
+    # get_jobs("intern",results_wanted=200,hours_old=2,country="worldwide",location="",is_remote=True)
+    all_jobs.drop_duplicates(inplace=True, ignore_index=True)
+    logging.warning(f"Total jobs no duplicates: {len(all_jobs)}")
+    all_api_key_used = filter_jobs(all_jobs, CV, km, good_fit_jobs)
+    if len(good_fit_jobs) > 0:
+        send_email(SENDER, RECEIVER, PASSWORD, good_fit_jobs)
+    else:
+        logging.warning("no good fit jobs")
+    if all_api_key_used == True:
+        return 429
+    
 
-get_jobs("devops", results_wanted=30, hours_old=2, country="egypt", location="cairo")
-get_jobs("backend", results_wanted=30, hours_old=2, country="egypt", location="cairo")
-get_jobs("software engineer",results_wanted=30,hours_old=2,country="egypt",location="cairo",)
-get_jobs("cloud", results_wanted=30, hours_old=2, country="egypt", location="cairo")
-get_jobs("site reliability engineer",results_wanted=30,hours_old=2,country="egypt",location="cairo")
-get_jobs("sre", results_wanted=30, hours_old=2, country="egypt", location="cairo")
-get_jobs("intern", results_wanted=30, hours_old=2, country="egypt", location="cairo")
-
-get_jobs("devops",results_wanted=200,hours_old=2,country="worldwide",location="",is_remote=True)
-get_jobs("backend",results_wanted=200,hours_old=2,country="worldwide",location="",is_remote=True)
-get_jobs("software engineer",results_wanted=200,hours_old=2,country="worldwide",location="",is_remote=True)
-get_jobs("intern",results_wanted=200,hours_old=2,country="worldwide",location="",is_remote=True)
-all_jobs.drop_duplicates(inplace=True, ignore_index=True)
-logging.warning(f"Total jobs no duplicates: {len(all_jobs)}")
-filter_jobs(all_jobs, CV, km, good_fit_jobs)
-if len(good_fit_jobs) > 0:
-    send_email(SENDER, RECEIVER, PASSWORD, good_fit_jobs)
-else:
-    print("no good fit jobs")
+if __name__ == "__main__":
+    main()
