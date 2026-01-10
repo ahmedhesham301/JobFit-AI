45687db9a8d4d7f2657e2a862f4a76671676a547
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2026-01-11 01:03:37 +0200

    commmented ai code execution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from jobs import getJobs
-from alert import send_email
-from filter import filter_jobs
+# from alert import send_email
+# from filter import filter_jobs
 import os
 import logging
 import pandas as pd
@@ -68,32 +68,32 @@ def main():
     all_jobs.drop_duplicates(subset=["job_url"], inplace=True, ignore_index=True)
     all_jobs = all_jobs.dropna(subset=["description"])
     s.jobs_no_duplicates = len(all_jobs)
-    t = datetime.now()
-    jobs_per_chunk = ceil(len(all_jobs) / 5)
-    jobs_chunks = [all_jobs[i : i + jobs_per_chunk] for i in range(0, len(all_jobs), jobs_per_chunk)]
-    kms = km.split(len(jobs_chunks))
-    print(f"number of jobs per chunk: {jobs_per_chunk}")
-    print(f"number of job chunks: {len(jobs_chunks)}")
-    print(f"number of kms: {len(kms)}")
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(filter_jobs, jobs_chunk, CV, km) for jobs_chunk,km in zip(jobs_chunks,kms)]
-        for future in concurrent.futures.as_completed(futures):
-            good_fit_jobs.extend(future.result())
+    # t = datetime.now()
+    # jobs_per_chunk = ceil(len(all_jobs) / 5)
+    # jobs_chunks = [all_jobs[i : i + jobs_per_chunk] for i in range(0, len(all_jobs), jobs_per_chunk)]
+    # kms = km.split(len(jobs_chunks))
+    # print(f"number of jobs per chunk: {jobs_per_chunk}")
+    # print(f"number of job chunks: {len(jobs_chunks)}")
+    # print(f"number of kms: {len(kms)}")
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
+    #     futures = [executor.submit(filter_jobs, jobs_chunk, CV, km) for jobs_chunk,km in zip(jobs_chunks,kms)]
+    #     for future in concurrent.futures.as_completed(futures):
+    #         good_fit_jobs.extend(future.result())
 
-    # all_api_key_used = filter_jobs(all_jobs, CV, km, good_fit_jobs)
-    s.filter_time = datetime.now() - t
-    if len(good_fit_jobs) > 0:
-        t = datetime.now()
-        send_email(SENDER, RECEIVER, PASSWORD, good_fit_jobs)
-        s.email_time = datetime.now() - t
-    else:
-        logging.warning("no good fit jobs")
+    # # all_api_key_used = filter_jobs(all_jobs, CV, km, good_fit_jobs)
+    # s.filter_time = datetime.now() - t
+    # if len(good_fit_jobs) > 0:
+    #     t = datetime.now()
+    #     send_email(SENDER, RECEIVER, PASSWORD, good_fit_jobs)
+    #     s.email_time = datetime.now() - t
+    # else:
+    #     logging.warning("no good fit jobs")
 
-    s.end_time = datetime.now()
+    # s.end_time = datetime.now()
     s.print()
 
-    # if all_api_key_used == True:
-    #     return 429
+    # # if all_api_key_used == True:
+    # #     return 429
 
 
 if __name__ == "__main__":
