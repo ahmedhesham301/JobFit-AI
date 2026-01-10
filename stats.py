8bbb35e089824a8e7254fe062b01d206c31704f9
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2026-01-11 01:17:12 +0200

    removed code casing error just for testing

--- stats.py
+++ stats.py
@@ -11,7 +11,5 @@ class Stats:
             f"""Total jobs with duplicates: {self.jobs_duplicates}
 Total jobs no duplicates: {self.jobs_no_duplicates}
 Scraping time: {self.scraping_time}
-Filter_time: {self.filter_time}
-AVG request time: {self.filter_time / self.jobs_no_duplicates}
-Email time: {self.email_time}"""
+"""
         )
