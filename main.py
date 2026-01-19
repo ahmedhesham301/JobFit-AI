7f473df37016a58be92c266be4607f8088ea5f81
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2026-01-19 07:10:18 +0200

    refactor: remove unused files and update requirements

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from jobs import getJobs
 import os
 import logging
 import pandas as pd
-from key_manger import KeyManger
 from stats import Stats
 from datetime import datetime,timedelta
 import concurrent.futures
 from jobs_to_search import jobs
-from math import ceil
 import json
 
 logging.basicConfig(
@@ -24,7 +22,6 @@ workflow_runs_info = json.loads(os.getenv("last_run_info"))
 
 all_jobs = pd.DataFrame()
 good_fit_jobs = []
-km = KeyManger(api_keys)
 last_run_info = None
 with open("instruction.txt", "r") as f:
     CV = f.read()
@@ -75,33 +72,8 @@ def main():
     all_jobs = all_jobs[["title", "description", "job_url"]]
     all_jobs["description"] = all_jobs["description"].apply(clean_description)
     all_jobs.to_csv("/output/output.csv", index=False)
-    # print(all_jobs.memory_usage(deep=True).sum())
-    # t = datetime.now()
-    # jobs_per_chunk = ceil(len(all_jobs) / 5)
-    # jobs_chunks = [all_jobs[i : i + jobs_per_chunk] for i in range(0, len(all_jobs), jobs_per_chunk)]
-    # kms = km.split(len(jobs_chunks))
-    # print(f"number of jobs per chunk: {jobs_per_chunk}")
-    # print(f"number of job chunks: {len(jobs_chunks)}")
-    # print(f"number of kms: {len(kms)}")
-    # with concurrent.futures.ThreadPoolExecutor() as executor:
-    #     futures = [executor.submit(filter_jobs, jobs_chunk, CV, km) for jobs_chunk,km in zip(jobs_chunks,kms)]
-    #     for future in concurrent.futures.as_completed(futures):
-    #         good_fit_jobs.extend(future.result())
-
-    # # all_api_key_used = filter_jobs(all_jobs, CV, km, good_fit_jobs)
-    # s.filter_time = datetime.now() - t
-    # if len(good_fit_jobs) > 0:
-    #     t = datetime.now()
-    #     send_email(SENDER, RECEIVER, PASSWORD, good_fit_jobs)
-    #     s.email_time = datetime.now() - t
-    # else:
-    #     logging.warning("no good fit jobs")
-
-    # s.end_time = datetime.now()
+    
     s.print()
-
-    # # if all_api_key_used == True:
-    # #     return 429
 
 
 if __name__ == "__main__":
