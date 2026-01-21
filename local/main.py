6a40de60f2b0948b1de87ec46cad7c13294f7a32
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2026-01-21 17:36:54 +0200

    updated artifacts range

--- local/main.py
+++ local/main.py
@@ -19,7 +19,7 @@ total_jobs = 0
 with open("cv.txt", "r") as file:
     cv = file.read()
 
-artifacts = am.list_artifacts("mokagad/job")["artifacts"][0:1]
+artifacts = am.list_artifacts("mokagad/job")["artifacts"][0:10]
 
 print(f"Total artifacts: {len(artifacts)}")
 
