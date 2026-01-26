5a95d5900a6bfc6528a4562d24a28cd149f155e8
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2026-01-26 14:01:51 +0200

    update to find bug

--- local/main.py
+++ local/main.py
@@ -33,27 +33,27 @@ for artifact in artifacts:
             )
     am.delete_artifact("mokagad/job", artifact["id"])
 
-# Get unfiltered jobs and filter them
-unfiltered_jobs = db.get_jobs("unfiltered")
-for row in unfiltered_jobs:
-    ai_response = local_ai.generate(row[2], cv)
-    ai_response = json.loads(ai_response)
-    if ai_response["fitPercentage"] >= 65:
-        db.update_status(row[0], "good_fit_not_sent", ai_response["fitPercentage"])
-    else:
-        db.update_status(row[0], "bad_fit", ai_response["fitPercentage"])
-    print(f"{row[1]}\t{ai_response["fitPercentage"]}%")
+# # Get unfiltered jobs and filter them
+# unfiltered_jobs = db.get_jobs("unfiltered")
+# for row in unfiltered_jobs:
+#     ai_response = local_ai.generate(row[2], cv)
+#     ai_response = json.loads(ai_response)
+#     if ai_response["fitPercentage"] >= 65:
+#         db.update_status(row[0], "good_fit_not_sent", ai_response["fitPercentage"])
+#     else:
+#         db.update_status(row[0], "bad_fit", ai_response["fitPercentage"])
+#     print(f"{row[1]}\t{ai_response["fitPercentage"]}%")
 
 
-alert.send_email(
-    os.getenv("smtp_email"),
-    os.getenv("receiver_email"),
-    os.getenv("smtp_password"),
-    db.get_jobs("good_fit_not_sent"),
-)
+# alert.send_email(
+#     os.getenv("smtp_email"),
+#     os.getenv("receiver_email"),
+#     os.getenv("smtp_password"),
+#     db.get_jobs("good_fit_not_sent"),
+# )
 
 
-end_time = time.perf_counter()
-elapsed_time = end_time - start_time
-print(f"Elapsed time: {elapsed_time:.1f} seconds")
-# print(f"Total jobs filtered: {total_jobs}")
+# end_time = time.perf_counter()
+# elapsed_time = end_time - start_time
+# print(f"Elapsed time: {elapsed_time:.1f} seconds")
+# # print(f"Total jobs filtered: {total_jobs}")
