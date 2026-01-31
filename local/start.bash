d597dcb5e5c39de79388b0e165e7fc2bbe850abc
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2026-01-31 19:11:10 +0200

    testing

--- local/start.bash
+++ local/start.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git -C /JobFit-AI pull
+git -C /JobFit-AI pull >> /var/log/job.log 2>&1
 
-# /JobFit-AI/local/myenv/bin/python /JobFit-AI/local/main.py >> /var/log/job.log 2>&1 || true
+/JobFit-AI/local/myenv/bin/python /JobFit-AI/local/main.py >> /var/log/job.log 2>&1 || true
 
 # shutdown +1
 shutdown -c
